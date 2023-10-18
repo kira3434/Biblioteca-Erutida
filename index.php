2efa08c9659df2ea9aa1d1d3fe6d6474943f1e98
Jhonatas Reis <83553776+kira3434@users.noreply.github.com>
2023-10-18 14:53:41 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Biblioteca Erudita</title>
+    <link rel="stylesheet" href="css/home.css">
+</head>
+
+<body>
+    <div id="corpo">
+        <div class="topo">
+            <div class="logo">
+                <img src="imagens-home/logo-imagem.png" alt="">
+                <h1>Biblioteca Erudita</h1>
+            </div>
+        </div>
+        <div class="centro">
+            <div class="esquerda-centro">
+                <div class="texto">
+                    <h1>Seja bem vindo
+                        ao sistema inteligente
+                        da <span class="destaque">Biblioteca Erudita</span>
+                    </h1>
+                    <div class="link">
+                        <a href="#">Entrar Agora</a>
+                    </div>
+                </div>
+            </div>
+            <div class="direita-centro">
+                <img src="imagens-home/imagem-centro.png" alt="">
+            </div>
+        </div>
+        <div class="baixo">
+            <h2>
+                Seja bem vindo <br>
+                ao sistema inteligente <br>
+                <span class="destaque">Biblioteca Erudita</span>
+            </h2>
+            <div class="link-baixo">
+            <a href="#">Entrar Agora</a>
+            </div>
+        
+        </div>
+    </div>
+</body>
+
+</html>
